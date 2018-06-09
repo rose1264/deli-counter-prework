4e0ecc22da35d5ebd8c218a74478e67e5467b498
Shumei Huang <rose1264@gmail.com>
2018-06-09 13:04:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,3 +5,7 @@ def line(katz_deli)
     puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
   end
 end
+
+def take_a_number(katz_deli, name)
+    katz_deli.push(name)
+end
